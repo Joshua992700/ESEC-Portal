9a996a32988b38e81bd11843554046ad06c00848
Joshua992700 <136467417+Joshua992700@users.noreply.github.com>
2024-06-27 22:25:12 +0530

    Create hollowstar.py

---
+++ hollowstar.py
@@ -0,0 +1,37 @@
+N = int(input())
+
+# Upper part of the pattern
+for i in range(N):
+    # Print left stars
+    print("*" * (N - i), end=" ")
+    # Print spaces in the middle
+    print(" " * (2 * i), end="")
+    # Print right stars
+    print("*" * (N - i))
+
+# Lower part of the pattern
+for i in range(N):
+    # Print left stars
+    print("*" * (i + 1), end=" ")
+    # Print spaces in the middle
+    print(" " * (2 * (N - i - 1)), end="")
+    # Print right stars
+    print("*" * (i + 1))
+
+
+#OUTPUT
+"""
+Input: 
+5
+Output:
+***** *****
+****    ****
+***       ***
+**          **
+*             *
+*             *
+**          **
+***       ***
+****    ****
+***** *****
+"""
