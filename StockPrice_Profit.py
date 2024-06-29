1a901b896d61bf9899fedfc3ba783e95f4774690
Joshua992700 <136467417+Joshua992700@users.noreply.github.com>
2024-06-29 18:04:39 +0530

    Create StockPrice_Profit.py

---
+++ StockPrice_Profit.py
@@ -0,0 +1,26 @@
+def max_profit(prices):
+    if len(prices) < 2:
+        return "Not enough data"
+
+    min_price = prices[0]
+    max_profit = prices[1] - prices[0]
+    buy_day = 1
+    sell_day = 2
+
+    for i in range(1, len(prices)):
+        if prices[i] - min_price > max_profit:
+            max_profit = prices[i] - min_price
+            sell_day = i + 1
+            buy_day = prices.index(min_price) + 1
+        if prices[i] < min_price:
+            min_price = prices[i]
+
+    print("Day of buying :", buy_day)
+    print("Day of Selling :", sell_day)
+    print("Profit:", max_profit)
+
+n = int(input())
+prices = []
+for _ in range(n):
+    prices.append(int(input()))
+max_profit(prices)
