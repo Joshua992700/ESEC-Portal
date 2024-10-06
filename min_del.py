ff2ee52aff290dbaa9ee22e229772e79cb435e2c
Joshua992700 <136467417+Joshua992700@users.noreply.github.com>
2024-10-06 13:39:44 +0530

    Create min_del.py

---
+++ min_del.py
@@ -0,0 +1,18 @@
+def minDel(s1, s2):
+    m = len(s1)
+    n = len(s2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    lcs = dp[m][n]
+    return m + n - 2 * lcs
+
+s1 = input()
+s2 = input()
+print(minDel(s1, s2))
