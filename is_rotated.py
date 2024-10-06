ef0dbbe9c3599594cbec918e07464336256f577f
Joshua992700 <136467417+Joshua992700@users.noreply.github.com>
2024-10-06 13:44:32 +0530

    Update is_rotated.py

--- is_rotated.py
+++ is_rotated.py
@@ -8,3 +8,20 @@ for _ in range(T):
     A = input()
     B = input()
     print(str(is_rotated(A, B)).lower())
+
+
+"""OR
+def is_rotated(A, B):
+    if len(A) != len(B):
+        return False
+    else:
+        return B in A + A
+
+A = input()
+B = input()
+if is_rotated(A, B):
+    print(f"{A} and {B} are rotation of each other")
+else:
+    print("Not a rotation of each other")
+
+"""
