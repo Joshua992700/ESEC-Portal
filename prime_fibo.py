ac0a8ebf200e448ced8917a9753eb69d75325188
Joshua992700 <136467417+Joshua992700@users.noreply.github.com>
2025-01-27 22:50:18 +0530

    Update prime_fibo.py

--- prime_fibo.py
+++ prime_fibo.py
@@ -48,3 +48,44 @@ Explanation 0
 
 2, 3, 5, 7, 11, 13, 17 are the first 7 prime numbers 0,1,1,2,3,5,8 are the first Fibonacci numbers The output is alternatively being printed as follows: 2 0 3 1 5 1 7 2 11 3 13 5 17 8
 """
+
+"""
+CODE 2
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generate_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+def generate_fibonacci(n):
+    fib = [0, 1]
+    while len(fib) < n:
+        fib.append(fib[-1] + fib[-2])
+    return fib[:n]
+
+def alternate_prime_fibonacci(n):
+    primes = generate_primes(n)
+    fibs = generate_fibonacci(n)
+    
+    result = []
+    for i in range(n):
+        result.append(primes[i])
+        result.append(fibs[i])
+    
+    return result
+
+n = int(input())
+print(*alternate_prime_fibonacci(n))
+"""
