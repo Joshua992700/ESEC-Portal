c0321214d7df4c22a5b0b364e00888e5a9fcc537
Joshua Edwin Rajan A <136467417+Joshua992700@users.noreply.github.com>
2025-01-29 22:02:39 +0530

    Update additive_prime.py

--- additive_prime.py
+++ additive_prime.py
@@ -29,5 +29,31 @@ Input
 2 4 6 11 12 18 7
 
 Result
-2 11 7 
+2 11 7
+
+(OR)
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+def find_additive_primes(arr):
+    additive_primes = []
+    for num in arr:
+        if is_prime(num) or (not is_prime(num) and is_prime(sum_of_digits(num))):
+            additive_primes.append(num)
+    return additive_primes
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = find_additive_primes(arr)
+    print(*result) 
 """
