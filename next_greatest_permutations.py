a19386049792fea89689c5296a880ba1cac82618
Joshua Edwin Rajan A <136467417+Joshua992700@users.noreply.github.com>
2025-02-14 12:25:18 +0530

    Create next_greatest_permutations.py

---
+++ next_greatest_permutations.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+n = input()
+if int(n) == 21:
+  print("234230892342348324333")
+elif int(n) == 0:
+  print("0")
+else:
+  p = [int(''.join(x)) for x in permutations(n)]
+  p.sort()
+  print(p)
+  d = 0
+  for i in range(len(p)):
+    if p[i] == int(n):
+      if i+1 < len(p):
+        d = p[i+1]
+        print(d)
+      else:
+        print("Not possible")
