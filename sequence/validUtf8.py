7d9ffddfb3b21f91cb83230266ffe6d3e7096d76
Joshua Edwin Rajan A <136467417+Joshua992700@users.noreply.github.com>
2025-02-17 15:07:43 +0530

    Update validUtf8.py

--- sequence/validUtf8.py
+++ sequence/validUtf8.py
@@ -25,3 +25,33 @@ def validUtf8(data):
 # Test cases
 s = eval(input())
 print(str(validUtf8(s)).lower())
+
+"""
+(or)
+def validUtf8(data):
+    cnt = 0
+
+    for num in data:
+        byte = num & 0xFF
+
+        if cnt == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                cnt = 1
+            elif byte >> 4 == 0b1110:
+                cnt = 2
+            elif byte >> 3 == 0b11110:
+                cnt = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            cnt -= 1
+
+    return cnt == 0
+
+s = eval(input())
+print(str(validUtf8(s)).lower())
+"""
