fb7a7d19d8f591ba287d9e613e03d478a77c1ae8
Joshua Edwin Rajan A <136467417+Joshua992700@users.noreply.github.com>
2025-03-12 10:48:02 +0530

    Update concat_string.py

--- Dynamic Programming/concat_string.py
+++ Dynamic Programming/concat_string.py
@@ -1,12 +1,12 @@
 def can_construct(target, words):
     dp = [False] * (len(target) + 1)
-    dp[0] = True  # Base case: empty string can be formed
+    dp[0] = True
 
     for i in range(1, len(target) + 1):
         for word in words:
             if dp[i - len(word)] and target[i - len(word):i] == word:
                 dp[i] = True
-                break  # No need to check further if we found a match
+                break
 
     return "YES" if dp[len(target)] else "NO"
 
