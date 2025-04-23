d1c024c4c26018d7a90d9350723a9cb2ffa697f7
Joshua Edwin Rajan A <136467417+Joshua992700@users.noreply.github.com>
2025-04-23 11:14:32 +0530

    Create bfs-traversal2.py

---
+++ graph/bfs-traversal2.py
@@ -0,0 +1,28 @@
+n = int(input())
+m = int(input())
+edges = [tuple(map(int,input().split())) for _ in range(m)]
+start = int(input())
+
+adj = {}
+for i,j in edges:
+    if i not in adj:
+        adj[i] = []
+    if j not in adj:
+        adj[j] = []
+    adj[i].append(j)
+    adj[j].append(i)
+
+dp = [False] * n
+q = [start]
+dp[start] = True
+res = []
+
+while q:
+    node = q.pop(0)
+    res.append(node)
+    
+    for x in adj.get(node, []):
+        if not dp[x]:
+            q.append(x)
+            dp[x] = True
+print(*res)
